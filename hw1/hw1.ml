a905ba25fed52b4d96cccb8f031b6bb1be36085e
jbeneliezer <judahbeneliezer@gmail.com>
2020-09-24 10:33:30 -0400

    added recitation 4

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -1,6 +1,25 @@
 let rec pow x n =
-        if n = 0 then 1
-        else if n = 1 then x
-        else x * (pow x (n - 1));;
+    if n = 0 then 1
+    else if n = 1 then x
+    else x * (pow x (n - 1));;
 
-pow 10 2;;
+let rec float_pow x n = match n with
+    | 0 -> 1.0
+    | _ -> x *. (float_pow x (n - 1));;
+
+let rec compress lst = match lst with
+    | [] -> []
+    | h::[] -> [h]
+    | h::s::t -> if h = s then s::compress t else h::compress t;;
+
+let l = ["h"; "h"; "e"; "d"; "d"];;
+
+compress l;;
+
+let rec print_list_of_strings = function
+        | [] -> Printf.printf ""
+        | h::t -> begin
+                Printf.printf "%s", h;
+                print_list_of_strings t
+        end;;
+       
